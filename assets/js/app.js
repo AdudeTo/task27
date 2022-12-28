92fbc5b7708c1302ae4523b001849be3916f9fec
Vicho Vichev <vicho.vichev@ns-mediagroup.com>
2022-12-29 01:28:42 +0200

    add cosmetics

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,6 +36,11 @@ function printHTML(data) {
         if (movie.show.image.medium == null) {
             movie.show.image.medium = returnMessages.noMedia;
         }
+
+        movie.show.name = movie.show.name.replaceAll(' Show', ' show');
+        movie.show.name = movie.show.name.replace(/Robot/g, Function.prototype.call.bind(String.prototype.toUpperCase));
+        movie.show.summary = movie.show.summary.replace(/Robot/g, Function.prototype.call.bind(String.prototype.toUpperCase));
+
         
         item.innerHTML =
         `
