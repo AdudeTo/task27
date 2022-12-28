ea6366dc25f9a4ee430a310d90ddb9366c37f7d9
Vicho Vichev <vicho.vichev@ns-mediagroup.com>
2022-12-28 22:38:09 +0200

    print search result

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,67 @@
-let movieListURL = "https://api.tvmaze.com/search/shows?q=Stargate";
+const searchField = document.getElementById("searchInput");
+const moviesListHolder = document.getElementById("moviesListHolder");
+let movieListAPI = "https://api.tvmaze.com/search/";
+let movieSearchCriteria = "shows/";
+let movieSearchQuery, movieLastSearch, movieCallConstructor; coolDown = 500; coolDownLoading = 0;
+let minCharacters = 3;
+let returnMessages = [];
 
+returnMessages["notRated"] = "not rated yet";
 
-fetchmovieList((error, data) => {
-    if (error)
-        console.log(error);
-    //show loading
-    //retry after 10 s max 3~5 times
-    //if still error show OPS something is wrong !
-    else
-       // sortData(data);
-       console.log(data);
-
-});
+function printHTML(data) {
+    moviesListHolder.innerHTML = "";
+    let allItemsCont = data.length;
+    data.forEach(async (movie) => {
+        if (movie.show.rating.average == null) {
+            movie.show.rating.average = returnMessages["notRated"];
+        } else {
+            movie.show.rating.average = movie.show.rating.average + " stars";
+        }
+        let item = document.createElement("LI");
+        item.innerHTML =
+        `
+            <figure>
+                <img alt="${movie.show.name}" src="${movie.show.image.medium}">
+            </figure>
+            <h3>${movie.show.name}</h3>
+            <h4>Rating: <span>${movie.show.rating.average}</span></h4>
+            <h5>${movie.show.genres.join(', ')}</h5>
+            ${movie.show.summary}
+        `;
+        moviesListHolder.appendChild(item);
+    });
+}
 
 function fetchmovieList(callback) {
-    fetch(movieListURL)
+    fetch(movieCallConstructor)
         .then(response => response.json())
         .then(json => callback(null, json))
         .catch(error => callback(error, null))
 }
+
+function prepareAcall() {
+    let getSearchQuery = searchField.value;
+    if (searchField.value == movieLastSearch) {
+        return;
+    }
+    if ((getSearchQuery.length >= minCharacters) && !coolDownLoading) {
+        movieCallConstructor = `${movieListAPI}${movieSearchCriteria}?q=${getSearchQuery}`;
+        fetchmovieList((error, data) => {
+            if (error)
+                console.log(error);
+            else
+                movieLastSearch = searchField.value;
+                printHTML(data);
+        });
+        coolDownLoading = 1;
+        setTimeout(() => { coolDownLoading = 0; prepareAcall(); }, coolDown);
+
+    }
+}
+
+searchField.addEventListener("keyup", (event) => {
+    if (event.key == "Enter") {
+        coolDownLoading = 0;
+    }
+    prepareAcall();
+});
